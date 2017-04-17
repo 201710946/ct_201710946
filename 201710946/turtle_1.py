89ebb446d75c66aab1cbcc2ffc4d6caf61f9fe0e
201710946 <yoon36399@naver.com>
2017-04-17 16:01:40 +0900

    turtle_1.py

--- 201710946/turtle_1.py
+++
@@ -1,5 +0,0 @@
-import turtle
-wn = turtle.Screen()
-t1 = turtle.Turtle()
-wn.addshape("C:\\Users\\400T6B\\Desktop\\새 폴더\\rocketship.gif")
-t1.shape("C:\\Users\\400T6B\\Desktop\\새 폴더\\rocketship.gif")
