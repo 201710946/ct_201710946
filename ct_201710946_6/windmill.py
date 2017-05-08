3b3d5013e23f4d770f4c17d4f7e372eec255c7a4
201710946 <yoon36399@naver.com>
2017-05-08 17:33:34 +0900

    Add files via upload
    windmill

---
+++ ct_201710946_6/windmill.py
@@ -0,0 +1,29 @@
+﻿import turtle
+wn = turtle.Screen()
+t1 = turtle.Turtle()
+turnBy = 45
+
+oldpos = t1.pos()
+def shape(size) :
+	oldHead = t1.heading()
+	t1.fd(size)
+	t1.rt(90)
+	t1.fd(size)
+	t1.penup()
+	t1.setpos(oldpos)
+	t1.setheading(oldHead + turnBy)
+	t1.pendown()
+
+for count in range (8) :
+	shape(100)
+
+wn.exitonclick()
+
+#shape(100)
+#shape(100)
+#shape(100)
+#shape(100)
+#shape(100)
+#shape(100)
+#shape(100)
+#shape(100)
